763575922de1cc7dd7d1401aa6e762d0bbde5413
Siddam1 <116861309+Siddam1@users.noreply.github.com>
2022-11-16 15:02:09 +0530

    Add files via upload

---
+++ Python/Activities/test_Activity221.py
@@ -0,0 +1,21 @@
+import pytest
+def test_add():
+    num1=10
+    num2=20
+    sum=num1+num2
+    assert sum ==30
+def test_sub():
+    num1=40
+    num2=20
+    Sub=num1-num2
+    assert Sub ==20   
+def test_mul():
+    num1=40
+    num2=2
+    mult=num1*num2
+    assert mult ==80    
+def test_div():
+    num1=10
+    num2=2
+    div=num1/num2
+    assert div == 5        
