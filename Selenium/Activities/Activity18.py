b17a6f351731d145d18ab1f7d8af4e94481a96e6
Siddam1 <116861309+Siddam1@users.noreply.github.com>
2022-11-16 16:59:33 +0530

    Delete Activity18.py

--- Selenium/Activities/Activity18.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-dataframe=pd.read_csv("logindata.csv")
-print(dataframe["Usernames"])
-print(dataframe["Usernames"][2],dataframe["Passwords"][2])
-print(dataframe.sort_values("Usernames"))
-print(dataframe.sort_values("Usernames", ascending=False))
-
-	
