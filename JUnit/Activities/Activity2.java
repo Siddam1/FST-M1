adf306643ac522ff3fadd270a9f9341b894dd6db
Siddam1 <116861309+Siddam1@users.noreply.github.com>
2022-11-16 11:35:16 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertThrows;
+
+class ExpectedExceptionTest {
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10)
+        );
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
