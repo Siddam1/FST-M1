69ad1fc47915d947a32983847f4aa123a3a1b974
Siddam1 <116861309+Siddam1@users.noreply.github.com>
2022-11-18 18:57:36 +0530

    Add files via upload

---
+++ Cucumber/Activities/testrunner/Activity5Runner.java
@@ -0,0 +1,16 @@
+package testrunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "C:\\Users\\002EYK744\\IdeaProjects\\Fst_selenium\\src\\test\\java\\Features",
+        glue = {"stepsDefenition"},
+        tags = "@activity2"
+)
+
+public class Activity5Runner {
+    //empty
+}
